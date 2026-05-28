f800f779ad1cf00f2be518f58ae665106fb06ca1
dtg-lucifer <piushbasu@gmail.com>
2026-05-28 21:46:56 +0530

    fix: removed the old stale user module and kept the users module

--- src/modules/user/user.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const get_user_by_email_schema = z.object({
-	query: z.object({
-		email: z.string().email(),
-	}),
-});
-
-export type GetUserByEmailInput = z.infer<typeof get_user_by_email_schema>["query"];
